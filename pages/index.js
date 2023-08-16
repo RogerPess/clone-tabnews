b8b7ca3c9b7e5dad43fb3da2b1e483f6e804b25a
Roger da Silva Pessoa <133066479+RogerPess@users.noreply.github.com>
2023-08-16 17:09:58 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Aprendendo a codar passo a passo😎</h1>
+  return <h1>Aprendendo a codar passo a passo😎</h1>;
+}
+
+function test() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
